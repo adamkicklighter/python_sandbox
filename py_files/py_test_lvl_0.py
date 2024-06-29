9cf19d7fd125adb72303ea468d3b9b1dc4ff38e0
adamkicklighter <97848631+adamkicklighter@users.noreply.github.com>
2024-06-29 05:43:28 -0400

    Adding very beginner level python test questions

---
+++ py_files/py_test_lvl_0.py
@@ -0,0 +1,51 @@
+# [0]
+
+"""
+Write a Python function called `is_even` that takes an integer as input and returns `True` if the number is even and `False` otherwise.
+"""
+
+# Given
+
+def is_even(number):
+
+    return number % 2 == 0
+
+# Test cases
+# print(is_even(4))  # Output: True
+# print(is_even(7))  # Output: False
+
+# Expected
+
+## Matched
+
+"""
+Write a python function called `sum_of_list` that takes a list of numbers as input and returns the sum of all the numbers in the list.
+"""
+
+# Given
+
+def sum_of_list(numbers):
+
+    return sum(numbers)
+
+# print(sum_of_list([1, 2, 3, 4]))  # Output: 10
+# print(sum_of_list([10, -2, 5]))   # Output: 13
+
+# Expected
+
+## Matched
+
+"""
+Write a Python function called `reverse_string` that takes a string as input and returns the string reversed.
+"""
+
+# Given
+
+this_str = "hello"
+
+print(*this_str)
+
+#print(reverse_string("hello"))  # Output: "olleh"
+#print(reverse_string("Python")) # Output: "nohtyP"
+
+#TBD
